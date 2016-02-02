2dc7f5beb98de37e17b5dad85ac1fca3983c5413
Spiros Gerokostas <spiros.gerokostas@gmail.com>
2016-02-02 12:36:49 +0200

    initial commit

---
+++ domain.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"net"
+	"strings"
+)
+
+type Domain struct {
+	Name string
+}
+
+func NewDomain(name string) *Domain {
+	return &Domain{
+		Name: name,
+	}
+}
+
+func (s *Domain) Exists() (bool, error) {
+	const whoisServer string = "com.whois-servers.net"
+	conn, err := net.Dial("tcp", whoisServer+":43")
+	if err != nil {
+		return false, err
+	}
+	defer conn.Close()
+	conn.Write([]byte(s.Name + "\r\n"))
+	scanner := bufio.NewScanner(conn)
+	for scanner.Scan() {
+		fmt.Println(scanner.Text())
+		if strings.Contains(strings.ToLower(scanner.Text()), "no match") {
+			return false, nil
+		}
+	}
+	return true, nil
+}

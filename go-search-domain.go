ad30417dd288828d43a11e953bc5b1ccb06cfcc6
Spiros Gerokostas <spiros.gerokostas@gmail.com>
2016-02-02 16:11:40 +0200

    removing command line options

--- go-search-domain.go
+++ go-search-domain.go
@@ -3,52 +3,26 @@ package main
 import (
 	"fmt"
 	"log"
-	"os"
 	"time"
 
 	"github.com/codegangsta/cli"
-	"github.com/sger/go-search-domain/version"
 )
 
 var marks = map[bool]string{true: "Domain exists", false: "Domain is available"}
 
 func main() {
 
-	app := cli.NewApp()
-	app.Version = version.Version()
-	app.Name = "go-search-domain"
-	app.Usage = "go-search-domain --domain=www.golang.com"
-	app.Flags = []cli.Flag{
-		cli.StringFlag{
-			Name:  "domain",
-			Value: "",
-			Usage: "Search for a domain like www.apple.com",
-		},
+	d := NewDomain("www.apple.com")
+
+	exist, err := d.Exists()
+
+	if err != nil {
+		log.Fatalln(err)
 	}
 
-	app.Action = func(c *cli.Context) {
+	fmt.Println(marks[exist])
+	time.Sleep(1 * time.Second)
 
-		err := validateArgs(c)
-
-		if err != nil {
-			fmt.Println(err)
-		} else {
-			domain := c.String("domain")
-			fmt.Println(domain)
-			d := NewDomain(domain)
-
-			exist, err := d.Exists()
-
-			if err != nil {
-				log.Fatalln(err)
-			}
-
-			fmt.Println(marks[exist])
-			time.Sleep(1 * time.Second)
-		}
-	}
-
-	app.Run(os.Args)
 }
 
 func validateArgs(c *cli.Context) error {

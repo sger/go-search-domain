7ed386af4db7d5fc17d92f5f9be449491378010a
Spiros Gerokostas <spiros.gerokostas@gmail.com>
2016-02-02 13:54:21 +0200

    small fixes

--- go-search-domain.go
+++ go-search-domain.go
@@ -10,13 +10,6 @@ var marks = map[bool]string{true: "✓", false: "✗"}
 
 func main() {
 
-	/*s := spin.New()
-
-	for i := 0; i < 30; i++ {
-		fmt.Printf("\r  \033[36m\033[m %s ", s.Next())
-		time.Sleep(100 * time.Millisecond)
-	}*/
-
 	d := NewDomain("www.apple.com")
 
 	exist, err := d.Exists()
@@ -25,9 +18,6 @@ func main() {
 		log.Fatalln(err)
 	}
 
-	//fmt.Println(exist)
-
-	//s.Reset()
 	if exist {
 		fmt.Println("Domain exists")
 	} else {

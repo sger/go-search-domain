79f4c57a0b94bf2b6068415c92b1f0a554fd0c6e
Spiros Gerokostas <spiros.gerokostas@gmail.com>
2016-02-02 13:19:04 +0200

    adding spinner

--- go-search-domain.go
+++ go-search-domain.go
@@ -4,11 +4,20 @@ import (
 	"fmt"
 	"log"
 	"time"
+
+	"github.com/tj/go-spin"
 )
 
 var marks = map[bool]string{true: "✓", false: "✗"}
 
 func main() {
+
+	s := spin.New()
+
+	for i := 0; i < 30; i++ {
+		fmt.Printf("\r  \033[36mcomputing\033[m %s ", s.Next())
+		time.Sleep(100 * time.Millisecond)
+	}
 
 	d := NewDomain("www.apple.com")
 

7aaf80aef320df5027a7995d66a714299933730b
Spiros Gerokostas <spiros.gerokostas@gmail.com>
2016-02-02 15:20:45 +0200

    command line options

--- go-search-domain.go
+++ go-search-domain.go
@@ -3,12 +3,35 @@ package main
 import (
 	"fmt"
 	"log"
+	"os"
 	"time"
+
+	"github.com/brettweavnet/gosync/version"
+	"github.com/codegangsta/cli"
 )
 
 var marks = map[bool]string{true: "Domain exists", false: "Domain is available"}
 
 func main() {
+
+	app := cli.NewApp()
+	app.Version = version.Version()
+	app.Name = "go-search-domain"
+	app.Usage = "go-search-domain DOMAIN"
+	app.Flags = []cli.Flag{
+		cli.StringFlag{
+			Name:  "domain",
+			Value: "",
+			Usage: "",
+		},
+	}
+
+	app.Action = func(c *cli.Context) {
+		domain := c.String("domain")
+		fmt.Println(domain)
+	}
+
+	app.Run(os.Args)
 
 	d := NewDomain("www.apple.com")
 
